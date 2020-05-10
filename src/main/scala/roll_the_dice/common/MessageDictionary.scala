2310ba7c06ff55ed57cc8ec1bfd0a6708434587a
Gabriele <gabriele.guerrini6@studio.unibo.it>
2020-05-10 12:18:15 +0200

    RollTheDice: fixed issue on messages serialization

--- src/main/scala/roll_the_dice/common/MessageDictionary.scala
+++ src/main/scala/roll_the_dice/common/MessageDictionary.scala
@@ -4,11 +4,15 @@ import java.io.Serializable
 
 object MessageDictionary {
 
+  @SerialVersionUID(0x1L)
   case class StartGame(assignedTurn: Turn, startingState: MatchState, goalPoints: Int) extends Serializable
 
+  @SerialVersionUID(0x2L)
   case object Roll extends Serializable
 
+  @SerialVersionUID(0x3L)
   case class NextTurn(currentTurn: Turn) extends Serializable
 
+  @SerialVersionUID(0x4L)
   case class Win(team: Team) extends Serializable
 }
